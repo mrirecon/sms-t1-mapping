5d9020508ae30ab3006ab852488fd43a546622d6
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2021-03-03 09:51:29 +0100

    add preliminary test script

---
+++ test.sh
@@ -0,0 +1,9 @@
+
+
+
+# verfied with BART v0.6.00-203-g660a00a
+bart nrmse -t 0.002 ${REFERENCE_RECON}/brain/vol_1/vol_1-a-sms5-brain-reco-001.coo vol_1-a-sms5-brain-reco-001.coo
+bart nrmse -t 0.002 ${REFERENCE_RECON}/brain/vol_1/vol_1-b-sms5-brain-reco-001.coo vol_1-b-sms5-brain-reco-001.coo
+bart nrmse -t 0.002 ${REFERENCE_RECON}/liver/vol_1/vol_1-a-sms3-liver-reco-00025.coo vol_1-a-sms3-liver-reco-00025.coo
+bart nrmse -t 0.002 ${REFERENCE_RECON}/liver/vol_1/vol_1-b-sms3-liver-reco-00025.coo vol_1-b-sms3-liver-reco-00025.coo
+
